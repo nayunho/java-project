bff6a1f5eb5ee6f7bb17b04fda76373a9e1a5ee2
nayunho <ring147@naver.com>
2021-09-24 21:16:32 +0900

    create java project

---
+++ src/test1/test_1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test1;
+
+/**
+ * @author ³ªÀ±È£
+ *
+ */
+public class test_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
