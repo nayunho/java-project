6b2c509b578bcf2f3b963ae2e593084515867777
nayunho <ring147@naver.com>
2021-09-24 21:26:34 +0900

    create student, carExam

---
+++ src/test1/carExam.java
@@ -0,0 +1,18 @@
+package test1;
+
+public class carExam {
+	String name;
+	String price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
---
+++ src/test1/student.java
@@ -0,0 +1,26 @@
+package test1;
+
+public class student {
+	String name;
+	String dept;
+	int grade;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+}
