60ade16a24f1d4e2e2289135e4d2e178a27fcee8
nayunho <ring147@naver.com>
2021-09-24 21:17:29 +0900

    update java

--- src/test1/test_1.java
+++ src/test1/test_1.java
@@ -14,7 +14,7 @@ public class test_1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git");
+		System.out.println("hello eclips");
 
 	}
 
